475cc5cfaf45e528f7585ced91bc7b56cc37a119
italo araya <iaraya@procesac.com>
2015-02-11 12:25:07 -0300

    Primera etapa de ObraRed a CodeIgniter
    .- la plantilla solo contiene "plantilla" y Cabecera

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -82,96 +82,89 @@ class Welcome extends CI_Controller {
         } else {
             switch ($seccion) {
                 case "Inicio":
-                    $var['variable'] = 'Inicio';
+                    $var['variable'] = 'inicio'; //solo para verificar los case..
                     $this->load->view('plantilla', $var);
-                    $this->load->view('cabecera');
                     break;
-                    switch ($seccion) {
-                        case "Inicio":
-                            $var['variable'] = 'inicio';//solo para verificar los case..
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Servicios":
-                            $var['variable'] = 'Servicios';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Nosotros":
-                            $var['variable'] = 'Nosotros';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Vision":
-                            $var['variable'] = 'Vision';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Mision":
-                            $var['variable'] = 'Mision';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Contacto":
-                            $var['variable'] = 'Contacto';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Presupuesto":
-                            $var['variable'] = 'Presupuesto';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Login":
-                            $var['variable'] = 'Login';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Registro":
-                            $var['variable'] = 'Registro';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Perfil":
-                            $var['variable'] = 'Perfil';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "ResBuscar":
-                            $var['variable'] = 'ResBuscar';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Buscar":
-                            $var['variable'] = 'Buscar';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Agrega_Materiales":
-                            $var['variable'] = 'Agrega_Materiales';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Cotizacion":
-                            $var['variable'] = 'Cotizacion';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "Codigo":
-                            $var['variable'] = 'Codigo';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "muestra_presupuestos":
-                            $var['variable'] = 'muestra_presupuestos';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "presupuestosAntiguos":
-                            $var['variable'] = 'presupuestosAntiguos';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "mantenedorMateriales":
-                            $var['variable'] = 'mantenedorMateriales';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "detallesMaterial":
-                            $var['variable'] = 'detallesMaterial';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "mantenedorUsuarios":
-                            $var['variable'] = 'mantenedorUsuarios';
-                            $this->load->view('plantilla', $var);
-                            break;
-                        case "detallesPersona":
-                            $var['variable'] = 'detallesPersona';
-                            $this->load->view('plantilla', $var);
-                            break;
-                    }
+                case "Servicios":
+                    $var['variable'] = 'Servicios';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Nosotros":
+                    $var['variable'] = 'Nosotros';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Vision":
+                    $var['variable'] = 'Vision';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Mision":
+                    $var['variable'] = 'Mision';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Contacto":
+                    $var['variable'] = 'Contacto';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Presupuesto":
+                    $var['variable'] = 'Presupuesto';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Login":
+                    $var['variable'] = 'Login';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Registro":
+                    $var['variable'] = 'Registro';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Perfil":
+                    $var['variable'] = 'Perfil';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "ResBuscar":
+                    $var['variable'] = 'ResBuscar';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Buscar":
+                    $var['variable'] = 'Buscar';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Agrega_Materiales":
+                    $var['variable'] = 'Agrega_Materiales';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Cotizacion":
+                    $var['variable'] = 'Cotizacion';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "Codigo":
+                    $var['variable'] = 'Codigo';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "muestra_presupuestos":
+                    $var['variable'] = 'muestra_presupuestos';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "presupuestosAntiguos":
+                    $var['variable'] = 'presupuestosAntiguos';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "mantenedorMateriales":
+                    $var['variable'] = 'mantenedorMateriales';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "detallesMaterial":
+                    $var['variable'] = 'detallesMaterial';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "mantenedorUsuarios":
+                    $var['variable'] = 'mantenedorUsuarios';
+                    $this->load->view('plantilla', $var);
+                    break;
+                case "detallesPersona":
+                    $var['variable'] = 'detallesPersona';
+                    $this->load->view('plantilla', $var);
+                    break;
             }
         }
         $this->load->view('cabecera');
