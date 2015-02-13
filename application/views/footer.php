2cd8d57831cba11a1764e5918cc93b437436f33e
NotebookRC <NotebookRC@NotebookRC-PC>
2015-02-12 23:05:58 -0300

    El footer se ve como corresponde

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,41 @@
 <!-- Footer -->
+<!-- Footer -->
 <div class="row">&nbsp;</div>
 <div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="row">&nbsp;</div>
+<div class="navbar navbar-default navbar-fixed-bottom color_footer">
+    <div class="container">
+        <div class="col-lg-12">&nbsp;</div>
+        <div class="col-lg-9">
+            <div class="col-lg-3">
+                <a href="?sec=Inicio"><img src="<?php base_url();?>assets/img/LOGO_2.png" width="120px"/></a><!-- logo ObraRed -->
+            </div>
+            <div class="col-lg-6">
+                <h6>
+                    ObraRed&reg; Chile | Todos los derechos reservados<br/>
+                    Teléfono: (2) 2 33 44 555<br/>
+                    E-mail: <a href="obraredchile@gmail.com">obraredchile@gmail.com</a>
+                </h6>
+            </div>
+        </div>
+        <div class="col-lg-3 ">
+            <div class='social-icons' id='social-iconos'>
+                <ul>
+                    <li class='social_icon'><a href='https://www.facebook.com/pages/ObraRed-Chile/529488350496097?ref=hl' target="_black"><img border='0' src='<?php base_url();?>assets/img/sfacp.png'/></a></li>
+                    <li class='social_icon'><a href='#'><img border='0' src='<?php base_url();?>assets/img/stwcp.png'/></a></li>
+                    <li class='social_icon'><a href='#'><img border='0' src='<?php base_url();?>assets/img/sgocp.png'/></a></li>
+                </ul>
+                <!--</h6>-->
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="<?php base_url();?>assets/css/bootstrap-theme.min.css">
         <script src="<?php base_url();?>assets/js/jquery-1.11.2.min.js"></script>
         <script src="<?php base_url();?>assets/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="<?php base_url();?>assets/css/footer.css">
         <!--<script type="text/javascript" src="Controlador/Javascript/funciones.js"></script>-->
     </head>
     <body>
